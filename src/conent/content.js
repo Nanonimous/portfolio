1fcd4601a89cc46455b9e987c03c93d7cb00a0d9
nanonimous <cocnivaas@gmail.com>
2025-12-29 06:56:30 +0530

    first commit

---
+++ src/conent/content.js
@@ -0,0 +1,170 @@
+
+
+export const reference = [
+    {
+        "icon":`ri-linkedin-box-fill`,
+        "image":"/images/linked_in.png",
+        "title":"Linked in",
+        "font":"1.6em",
+        "link":"https://www.linkedin.com/in/nivaasb/"
+    },
+    {
+        
+        "title":"GitHub",
+        "image":"/images/github.png",
+        "font":"1.6em",
+        "link":"https://github.com/Nanonimous"
+        
+        
+    },
+    {
+        "icon":"",
+        "title":"Email",
+        "image":"/images/mail.png",
+        "font":"1.6em",
+        "link":"mailto:nivaas23barath123@gamil.com"
+    }
+]
+
+
+
+
+export const project = [
+    {
+        "title":"SmilingStars (DayCare)",
+        "Stack":["Html 5","Css 3","Javascript","React js","Node js","Express js","Rest Api","Postgresql"],
+        "description":"Developed the frontend using React.js with a responsive UI for student admissions and program selection (DayCare, Bharatanatyam, Carnatic Music, Violin, Arts, Tabla & Mridangam, Hindi).",
+        "image":'/images/smilingstars.png',
+        "links":[{
+            "name":"Visit Site",
+            "link":"https://smilingstarsdaycare.in/"
+        }],
+        "live":".Live"
+    },
+    {
+        "title":"Department Site",
+        "Stack":["Html 5","Css 3","Javascript","React js","Node js","Express js","Rest Api","Mongo db"],
+        "description":"I contributed to building a dynamic and responsive website for the Computer Science & Engineering (ICB) department at MVIT. The platform showcases key departmental information including faculty profiles, student achievements, publications, events, academic calendar, timetable, and resources. The site features role-based authentication for admins and students, enabling secure access to personalized content.",
+        "image":'/images/image.png',
+        "links":[{
+            "name":"Visit Site",
+            "link":"https://mvitcseicb.in"
+        }],
+        "live":".Live"
+    },
+    {
+        "title":"Zenotion",
+        "Stack":["Html 5","Css 3","Javascript","Ejs","Node js","Express js","Rest Api","Postgresql",],
+        "description":"This website facilitates the sharing of educational resources between teachers and students. It includes personal spaces for students to store and manage their resources. Through this project, I gained valuable experience in both front-end and back-end development.",
+        "image":'/images/zenotion.png',
+        "links":[{
+            "name":"Youtube",
+            "link":"https://www.youtube.com/watch?v=3cw0xUy66y0"
+        },{
+            "name":"GitHub Code",
+            "link":"https://github.com/Zenotion/Zenotion"
+        }],
+       
+    },
+    {
+        "title":"Smart Healthcare System for skin Disease Prediction with Encrypted EMR Storage Using Blockchain and EfficientNetB0",
+        "Stack":["Html 5","Css 3","Javascript","Flask","Smart contract","ethereum","SQLite","CryptoChaos Encryption","TensorFlow","EfficientNetB0","Ganache","NumPy","Pandas","Matplotlib"],
+        "description":`Developed a secure healthcare platform for EMR management using Ethereum blockchain, IPFS, and CryptoChaos encryption, ensuring data privacy and tamper-proof access. Integrated EfficientNetB0-based skin disease prediction and built the system using Python, Flask, TensorFlow, and SQLite.`,
+        "image":'/images/app.jpg',
+        "links":[{
+            "name":"GitHub Code",
+            "link":"https://github.com/hirthickgowtham/Weather-information-site"
+        }]
+    },
+    {
+        "title":"Melakartha 72",
+        "Stack":["Html 5","Css 3","Typescript","React Native"],
+        "description":`Developed an Android application to identify and explore Carnatic ragas from the 72 Melakarta system, featuring a custom virtual keyboard for note-based raga detection and detailed raga pages with audio playback to support music learning. Role: React Native Developer.`,
+        "image":'/images/app.jpg',
+        "links":[{
+            "name":"GitHub Code",
+            "link":"https://github.com/hirthickgowtham/Weather-information-site"
+        }]
+    },
+    
+    
+]
+
+
+
+export const tech = [[{
+    "name":"Html 5",
+    "icon":"/portfolio-site/images/html.png"
+},
+{
+    "name":"CSS 3",
+    "icon":"/portfolio-site/images/css.png"
+},
+{
+    "name":"Java Script",
+    "icon":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/600px-JavaScript-logo.png"
+},
+{
+    "name":"React js",
+    "icon":"https://react.dev/favicon.ico"
+},
+{
+    "name":"Redux js",
+    "icon":"https://redux-toolkit.js.org/img/favicon/favicon.ico"
+},
+{
+    "name":"Node js",
+    "icon":"https://nodejs.org/static/images/favicons/favicon.png"
+},
+{
+    "name":"Express js",
+    "icon":"https://expressjs.com/images/favicon.png"
+},
+{
+    "name":"postgresql",
+    "icon":"/portfolio-site/images/postgresql.png"
+},
+{
+    "name":"MongoDB",
+    "icon":"https://www.mongodb.com/favicon.ico"
+},
+{
+    "name":"Python",
+    "icon":"https://www.python.org/favicon.ico"
+},
+{
+    "name":"Redis",
+    "icon":"/portfolio-site/images/redis.png"
+}
+],
+[{
+    "name":"VS code",
+    "icon":"/portfolio-site/images/vscode.png"
+    
+},
+{
+    "name":"Postman",
+    "icon":"https://www.postman.com/_ar-assets/images/favicon-1-32.png"
+},
+{
+    "name":"GIT",
+    "icon":"https://git-scm.com/favicon.ico"
+},
+{
+    "name":"GitHub",
+    "icon":"/portfolio-site/images/github (2).png"
+}
+
+]]
+
+
+export const contact = [
+    {
+        "icon":"/portfolio-site/images/github.png",
+        "link":"https://github.com/Nanonimous"
+    },
+    {
+        "icon":"/portfolio-site/images/linked_in.png",
+        "link":"https://www.linkedin.com/in/nivaasb/"
+    }
+]
